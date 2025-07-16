fe50cb8f7c47078cc8543b6cb133f802118d0d54
Olaiwonismail <olaiwonismail@gmail.com>
2025-07-16 01:10:32 +0100

    to production

--- research_hub/backend/app/routers/notifications.py
+++ research_hub/backend/app/routers/notifications.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy import distinct
 
-from app import schemas
+from .. import schemas
 from .. import models
 from app.auth import verify_access_token
 from app.database import AsyncSessionLocal
